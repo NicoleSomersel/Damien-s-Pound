2bce2b040fe4404a53699d1234804332a2317249
Nicole Somersel <nicole.somersel@gmail.com>
2025-02-22 09:21:47 -0500

    Update words.js

--- words.js
+++ words.js
@@ -1,42 +1,33 @@
 const words= [
   {
-    word:"Juxtaposition",
-    hint:"the fact of placing two things side by side, usually in contrast"
+    word:"Juxtaposition",hint:"the fact of placing two things side by side, usually in contrast"
   },
   {
-    word:"Diction",
-    hint:"the clearness and effectiveness of enunciation when speaking, or choice of words"
+    word:"Diction",hint:"the clearness and effectiveness of enunciation when speaking, or choice of words"
   },
   {
-    word:"Paraphrasing",
-    hint:"express the meaning of (the writer or speaker or something written or spoken) using different words, especially to achieve greater clarity"
+    word:"Paraphrasing",hint:"express the meaning of (the writer or speaker or something written or spoken) using different words, especially to achieve greater clarity"
   },
   {
-    word:"Asylum",
-    hint:"protection granted by a country for a political refugee who has left their native country, or a place of safety"
+    word:"Asylum",hint:"protection granted by a country for a political refugee who has left their native country, or a place of safety"
   },
   {
-    word:"Apathy",
-    hint:"having no emotion, feeling, or concern"
+    word:"Apathy",hint:"having no emotion, feeling, or concern"
   },
   {
-    word:"Annotation",
-    hint:"a note of explanation or comment added to a text or diagram"
+    word:"Annotation",hint:"a note of explanation or comment added to a text or diagram"
   },
   {
-    word:"Brainstorming",
-    hint:"a spontaneous group discussion to produce ideas and ways of solving problems,a sudden clever idea"
+    word:"Brainstorming",hint:"a spontaneous group discussion to produce ideas and ways of solving problems,a sudden clever idea"
   },
   {
-    word:"scene",
-    hint:"a stage setting, a real or imaginary prospect suggesting a stage setting"
+    word:"scene",hint:"a stage setting, a real or imaginary prospect suggesting a stage setting"
   },
   {
     word:"thesis",
     hint:"a long essay or dissertation involving personal research, written by a candidate for a college degree:"
   },
   {
-    word:"perspective",
-    hint:"a mental view or prospect, a visible scene"
+    word:"perspective",hint:"a mental view or prospect, a visible scene"
   },
 ]

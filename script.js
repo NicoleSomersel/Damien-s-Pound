f67fe853efeae43000796a921a649f671ad8242f
Nicole Somersel <nicole.somersel@gmail.com>
2025-02-22 08:03:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ let correctWord, timer;
 const initTimer = () => {
   clearInterval(timer);
   timer = setInterval(()  => {
-    if maxtime > 0{
+    if (maxtime> 0){
       maxtime--;
       return timeText.innerText = maxtime;
     }
     clearInterval(timer);
     alert('Oops Time is Over' ${userword.toUpperCase()} 'was the correct!');
     initGame();
-  }, 1000)
-}
+  }, 1000);
+};
 
 
 const initGame = () =>{
@@ -30,7 +30,7 @@ const initGame = () =>{
   let randomObj = words[math.floor(math.random() * words.length)];
   let wordArray = randomObj.word.split("");
   for(let i = wordArray.length; i>0; i--){
-    let j = math.floor.math.random()* (i+1);
+    let j = math.floor(math.random()* (i+1));
 
     wordArray[i], wordArray[j] = wordArray[j], wordArray[i];
   }
@@ -40,17 +40,17 @@ const initGame = () =>{
   correctWord = randomObj.word.toLowerCase;
   console.log(randomObj);
   inputField.value = "";
-  intputField.setAttribute= ("Maximum length: ", correctWord.length)
+  intputField.setAttribute= ("Maximum length", correctWord.length);
 }
 initGame();
 const checkWord = () =>{
   let userWord = inputField.value.toLowerCase();
-    if(!userWord) return alert('Thats not a word.');
-  if(userWord != correctWord) return alert('Oh no' ${userword} 'is not correct! Try again.');
+  if(!userWord) return alert('Thats not a word.');
+  if(userWord !== correctWord) return alert('Oh no' ${userword} 'is not correct! Try again.');
   alert('Yippee' ${userword.toUpperCase()} 'is correct!');
   initGame():
 }
 
 refreshBtn.addEventListner('click', initGame());
 refreshBtn.addEventListner('click', checkWord());
-
+initGame():

f372d1797c44527ba1fc52fb9061e5fe2493666d
Nicole Somersel <nicole.somersel@gmail.com>
2025-02-22 06:38:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+const wordText =document.querySelector(".word");
+hintText =document.querySelector(".word");
+
 const initGame = () =>{
   let randomObj = words[math.floor(math.random() * words.length)];
+  let wordArray = randomObj.word.split("");
+  for(let i = wordArray.length; i>0; i--){
+    let j = math.floor.math.random()* (i+1);
+
+    wordArray[i], wordArray[j] = wordArray[j], wordArray[i];
+  }
+
+  wordText.innerText = wordArray.join("");
+  console.log(randomObj);
 }
+initGame();

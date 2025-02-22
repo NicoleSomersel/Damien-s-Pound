af89f738cb8f7d4c21e5d23afcd8dc3b44ecefe4
Nicole Somersel <nicole.somersel@gmail.com>
2025-02-22 08:26:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-
+console.log("words array", words);
 const wordText =document.querySelector(".word");
 hintText =document.querySelector(".hint span");
 refreshBtn =document.querySelector(".refresh"),
 inputField =document.querySelector("input"),
 timeText =document.querySelector(".time b"),
-  
 checkBtn =document.querySelector(".check");
 
 
 
 let correctWord, timer;
 
-const initTimer = () => {
+const initTimer = (maxtime) => {
   clearInterval(timer);
   timer = setInterval(()  => {
     if (maxtime> 0){
       maxtime--;
       return timeText.innerText = maxtime;
-    }
+    }else{
     clearInterval(timer);
     alert('Oops Time is Over' ${userword.toUpperCase()} 'was the correct!');
     initGame();
+    }
   }, 1000);
 };
 
 
 const initGame = () =>{
   initTimer(30);
-  let randomObj = words[math.floor(math.random() * words.length)];
+  let randomObj = words[Math.floor(Math.random() * words.length)];
+  console.log(randomObj);
   let wordArray = randomObj.word.split("");
   for(let i = wordArray.length; i>0; i--){
     let j = math.floor(math.random()* (i+1));
 
-    wordArray[i], wordArray[j] = wordArray[j], wordArray[i];
+    [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[I]];
   }
 
   wordText.innerText = wordArray.join("");
   hintText.innerText= randomObj.hint;
   correctWord = randomObj.word.toLowerCase;
-  console.log(randomObj);
   inputField.value = "";
-  intputField.setAttribute= ("Maximum length", correctWord.length);
-}
-initGame();
+  inputField.setAttribute= ("Maximum length", correctWord.length);
+};
+
 const checkWord = () =>{
   let userWord = inputField.value.toLowerCase();
   if(!userWord) return alert('Thats not a word.');
